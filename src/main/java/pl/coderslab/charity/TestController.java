75c6ecced91398d85ffeaf858a92315fc22a2784
MarcinKubik <kubikmmarcin@gmail.com>
2021-01-04 18:04:15 +0100

    Header and footer separated

---
+++ src/main/java/pl/coderslab/charity/TestController.java
@@ -0,0 +1,28 @@
+package pl.coderslab.charity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/form")
+    public String form(){
+        return "form";
+    }
+
+    @GetMapping("/formConfirmation")
+    public String formConfirmation(){
+        return "form-confirmation";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
